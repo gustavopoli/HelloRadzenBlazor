67e8daa1d82ff279f271b0d5b584d91ab84ab60d
Gustavo Poli <gustavo.poli@outlook.com>
2023-12-30 16:47:16 -0800

    v0 Project
    Hello World and Form using Radzen

---
+++ HelloWorldRadzen/HelloWorldRadzen/Program.cs
@@ -0,0 +1,36 @@
+using HelloWorldRadzen.Client.Pages;
+using HelloWorldRadzen.Components;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRadzenComponents();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
